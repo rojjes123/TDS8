4e2812d2b86823c87639f551fed551d11a5a8516
ABHINAV TIWARI <125191334+rojjes123@users.noreply.github.com>
2023-04-19 13:31:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,10 @@ def find_largest(num1, num2, num3):
     """
     Find The Largest
     """
-    """
-    This function finds the largest among three given natural numbers.
-    """
+    ""
+    put uour numbers
+    ""
+    
     if num1 >= num2 and num1 >= num3:
         return num1
     elif num2 >= num1 and num2 >= num3:
@@ -20,9 +21,9 @@ def main():
     This function defines the Streamlit app UI and logic.
     """
     st.title("Find the largest among 3 natural numbers")
-    num1 = st.number_input("Enter the first natural number", value=0, step=1)
-    num2 = st.number_input("Enter the second natural number", value=0, step=1)
-    num3 = st.number_input("Enter the third natural number", value=0, step=1)
+    num1 = st.number_input("Enter the first  number", value=0, step=1)
+    num2 = st.number_input("Enter the second  number", value=0, step=1)
+    num3 = st.number_input("Enter the third  number", value=0, step=1)
 
     if st.button("Find the largest"):
         largest_num = find_largest(num1, num2, num3)
